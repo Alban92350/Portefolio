b94b582b86b79b719b11599b732b0ff611bdf818
Alban teissier <albanteissier191@gmail.com>
2021-02-18 20:17:32 +0100

    finalisation du burger

--- js/app.js
+++ js/app.js
@@ -4,40 +4,24 @@ const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
+  // hamburger.style.transform = "rotate(90deg)"
+  var a = navLinks.classList.contains("open");
+  if(!a){
+    hamburger.style.transform = "rotate(180deg)"
+    hamburger.style.transition = "0.8s";
+  }else{
+    hamburger.style.transform = "rotate(0deg)"
+  }
   navLinks.classList.toggle("open");
   links.forEach(link => {
     link.classList.toggle("fade");
   });
 });
 
+// var a = navLinks.classList.contains('open');
 
 
-// Title
-const text = document.querySelector('.title');
-const strText = text.textContent;
-const splitText = strText.split("");
+ 
 
 
-text.textContent = "";
 
-for(i = 0; i < splitText.length; i++){
-    text.innerHTML += "<span>" + splitText[i] + "</span>";
-}
-
-let char = 0;
-let timer = setInterval(onTick, 50); 
-
-function onTick(){
-    const span = document.querySelectorAll('span')[char];
-    span.classList.add('fade');
-    char ++ 
-    if(char === splitText.length){
-        complete();
-        return;
-    }
-}
-
-function complete(){
-    clearInterval(timer);
-    // timer = null;
-}
